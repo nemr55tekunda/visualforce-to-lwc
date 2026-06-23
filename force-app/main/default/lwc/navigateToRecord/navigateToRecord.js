31919cdecd8ce9d5ef6d59bf323769064f742a40
Serpent Org-to-Org Deployment <deployment@serpent.local>
2026-06-23 18:27:37 +0300

    chore: sync target org metadata

--- force-app/main/default/lwc/navigateToRecord/navigateToRecord.js
+++ force-app/main/default/lwc/navigateToRecord/navigateToRecord.js
@@ -1,12 +1,23 @@
 import { LightningElement, api } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 
-export default class NavigateToRecord extends NavigationMixin(
-    LightningElement
-) {
+export default class NavigateToRecord extends NavigationMixin(LightningElement) {
     @api recordId;
-
+    @api label = 'View Record';
+    @api autoNavigate = false; // Optional: auto-navigate on load
+    
     connectedCallback() {
+        if (this.autoNavigate && this.recordId) {
+            this.performNavigation();
+        }
+    }
+    
+    navigate(event) {
+        event.preventDefault();
+        this.performNavigation();
+    }
+    
+    performNavigation() {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
             attributes: {
---
+++ force-app/main/default/lwc/testtwoo/testtwoo.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class Testtwoo extends LightningElement {}
