b911caf471ffa8a166b1966efc8abc994154c0f3
Serpent Compare Org and Deploy <deployment@serpent.local>
2026-08-06 07:51:10 +0000

    chore: sync target org metadata

--- force-app/main/default/lwc/paginator/paginator.js
+++ force-app/main/default/lwc/paginator/paginator.js
@@ -1,16 +1,8 @@
 import { LightningElement, api } from 'lwc';
 
-const MAX_ITEM_OFFSET = 2000;
-
 export default class Paginator extends LightningElement {
-    /** The current page number. */
-    @api pageNumber;
-
-    /** The number of items on a page. */
-    @api pageSize;
-
-    /** The total number of items in the list. */
-    @api totalItemCount;
+    @api previousButtonDisabled;
+    @api nextButtonDisabled;
 
     handlePrevious() {
         this.dispatchEvent(new CustomEvent('previous'));
@@ -18,24 +10,5 @@ export default class Paginator extends LightningElement {
 
     handleNext() {
         this.dispatchEvent(new CustomEvent('next'));
-    }
-
-    get currentPageNumber() {
-        return this.totalItemCount === 0 ? 0 : this.pageNumber;
-    }
-
-    get isNotFirstPage() {
-        return this.pageNumber > 1;
-    }
-
-    get isNotLastPage() {
-        return (
-            this.pageNumber < this.totalPages &&
-            this.pageNumber * this.pageSize < MAX_ITEM_OFFSET
-        );
-    }
-
-    get totalPages() {
-        return Math.ceil(this.totalItemCount / this.pageSize);
     }
 }
